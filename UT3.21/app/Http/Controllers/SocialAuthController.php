85b29239b3a7f99486df12ff41f58195a44bd8bc
jarrisonfor <yegoma@outlook.com>
2021-11-18 00:42:50 +0000

    restablecemos el redirect

--- UT3.21/app/Http/Controllers/SocialAuthController.php
+++ UT3.21/app/Http/Controllers/SocialAuthController.php
@@ -10,9 +10,7 @@ class SocialAuthController extends Controller
 {
     public function redirectToProvider($provider)
     {
-        return Socialite::driver($provider)
-            ->with(['response_type' => 'code', 'code' => '394a8bc98028f39660e53025de824134fb46313'])
-            ->redirect();
+        return Socialite::driver($provider)->redirect();
     }
 
     public function handleProviderCallback($provider)
