9390aec481ef45b19c8380ace977cc0796c6ce4d
jarrisonfor <yegoma@outlook.com>
2021-12-01 00:20:04 +0000

    validaciones

--- UT3.10/app/Http/Requests/UpdateAlumnoRequest.php
+++ UT3.10/app/Http/Requests/UpdateAlumnoRequest.php
@@ -26,9 +26,9 @@ class UpdateAlumnoRequest extends FormRequest
         return [
             'nombre' => 'string|min:3|max:255',
             'apellidos' => 'string|min:3|max:255',
-            'email' => 'string|email|max:255|unique:alumnos',
+            'email' => 'sometimes|string|email|max:255|unique:alumnos',
             'f_nacimiento' => 'date',
-            'c_postal' => 'string|min:1000|max:52999',
+            'c_postal' => 'numeric|min:1000|max:52999',
             'codigo' => 'regex:/[0-9]{4}-[A-Z]/|string|max:255',
         ];
     }
