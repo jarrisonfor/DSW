7e34b70b3f6bf1c8a2fdc4ccfb7b98073a82ad37
jarrisonfor <yegoma@outlook.com>
2021-12-16 19:27:41 +0000

    actividad 3.23 sin empezar

---
+++ UT3.23/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Publicacion;
+use App\Models\Usuario;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Usuario::factory(50)->has(Publicacion::factory()->count(10))->create();
+    }
+}
---
+++ UT3.23/routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\UsuarioController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return redirect()->route('usuario.index');
+});
+
+Route::get('/usuario/borrar', [UsuarioController::class, 'destroyAll'])->name('usuario.delete');
+Route::get('/usuario/restaurar', [UsuarioController::class, 'restoreAll'])->name('usuario.restore');
+Route::get('/usuario/{usuario}/publicaciones', [UsuarioController::class, 'publicaciones'])->name('usuario.publicaciones');
+Route::resource('/usuario', UsuarioController::class);
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
