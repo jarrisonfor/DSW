dd48153c42b90f129842e5732da342fbb9afa0bf
jarrisonfor <yegoma@outlook.com>
2022-02-19 17:03:31 +0000

    adaptamos configuracion del mongodb para goormide

--- UT5.1/config/database.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    url: 'mongodb://localhost:27017',
-    auth: {
-        user: 'yeray',
-        pass: 'csas1234',
-        database: 'UT51'
-    }
-}
--- UT5.1/server.js
+++ UT5.1/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // create express app
 const app = express();
@@ -8,22 +9,20 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 // Configuring the database
-const dbConfig = require('./config/database.config.js');
-const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
 const opts = {
-    dbName: dbConfig.auth.database,
-    user: dbConfig.auth.user,
-    pass: dbConfig.auth.pass,
+    //dbName: dbConfig.auth.database,
+    //user: dbConfig.auth.user,
+    //pass: dbConfig.auth.pass,
     useNewUrlParser: true,
     useUnifiedTopology: true,
     bufferCommands: false,
 }
 
 // Connecting to the database
-mongoose.connect(dbConfig.url, opts).then(() => {
+mongoose.connect('mongodb://localhost:27017/UT51', opts).then(() => {
     console.log("Successfully connected to the database");
 }).catch(err => {
     console.log('Could not connect to the database. Exiting now...', err);
