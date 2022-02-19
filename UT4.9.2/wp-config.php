22e7d85317b1a61564a0ed06ecd2f6f13aca7775
jarrisonfor <yegoma@outlook.com>
2022-02-19 15:51:14 +0000

    terminado 4.9.2

--- UT4.9.2/wp-config.php
+++ UT4.9.2/wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'UT4.9' );
+define( 'DB_NAME', 'UT4.9.2' );
 
 /** MySQL database username */
 define( 'DB_USER', 'yeray' );
