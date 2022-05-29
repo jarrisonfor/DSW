292c860edec9ecb91366e6680cdffeaa6b3c728b
jarrisonfor <40749014+jarrisonfor@users.noreply.github.com>
2022-05-29 23:26:30 +0100

    cambiamos a env

--- Proyecto Navidad/app/Providers/AppServiceProvider.php
+++ Proyecto Navidad/app/Providers/AppServiceProvider.php
@@ -29,7 +29,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
-        if (Request::server('HTTP_X_FORWARDED_PROTO') == 'https') {
+        if(env('FORCE_HTTPS', false)) {
             URL::forceScheme('https');
         }
         Gate::before(function ($user) {
