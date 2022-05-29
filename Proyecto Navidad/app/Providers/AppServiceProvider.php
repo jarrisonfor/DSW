8f39ef7d2bf735df0bac0fdeafac0b6996ff451b
jarrisonfor <40749014+jarrisonfor@users.noreply.github.com>
2022-05-29 23:23:54 +0100

    intentamos adivinar si usa http o https

--- Proyecto Navidad/app/Providers/AppServiceProvider.php
+++ Proyecto Navidad/app/Providers/AppServiceProvider.php
@@ -27,13 +27,15 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
-
+        if (Request::server('HTTP_X_FORWARDED_PROTO') == 'https') {
+            URL::forceScheme('https');
+        }
         Gate::before(function ($user) {
             return $user->hasRole('admin') ? true : null;
         });
         Gate::after(function ($user) {
             return $user->hasRole('admin');
-         });
+        });
 
         Blade::stringable(function(\Illuminate\Support\Carbon $dateTime) {
             if ($dateTime->hour === 0 && $dateTime->minute === 0 && $dateTime->second === 0) {
